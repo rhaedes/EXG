eda150f69806d890a9511f85b40d32d00933ee74
dk\sje <sje@sitecore.net>
2016-06-08 12:54:53 +0200

    Dashboard update to return random values

--- code/Sitecore.EXG.Client/exgController.cs
+++ code/Sitecore.EXG.Client/exgController.cs
@@ -3,37 +3,42 @@ using Sitecore.Services.Infrastructure.Web.Http;
 
 namespace Sitecore.EXG.Client
 {
+  using System;
   using System.Web.Http;
 
-  [ServicesController("test")] 
-  
-    public class exgController: ServicesApiController
+  [ServicesController("ExgData")]
+  public class exgController : ServicesApiController
 
-    {
+  {
+    private static readonly Random getrandom = new Random();
+
     [HttpGet]
-      public string Dashboard()
-      {
-        var output = "{ data: { visitsPerChannel: [ " + "{ \"label\": \"LandingPages\", \"value\": getRandomNumber() },"
-                     + "{ \"label\": \"RefURLs\", \"value\": getRandomNumber() },"
-                     + "{ \"label\": \"Search\", \"value\": getRandomNumber() },"
-                     + "{ \"label\": \"Campaigns\", \"value\": getRandomNumber() },"
-                     + "{ \"label\": \"Other\", \"value\": getRandomNumber() }" + "]," + "monthlyDistribution: [" + "{"
-                     + "key: \"Monthly Distribution\"," + "values: ["
-                     + "{ \"label\": \"January\", \"value\": getRandomNumber() },"
-                     + "{ \"label\": \"February\", \"value\": getRandomNumber() },"
-                     + "{ \"label\": \"March\", \"value\":getRandomNumber() },"
-                     + "{ \"label\": \"April\", \"value\": getRandomNumber() },"
-                     + "{ \"label\": \"Maj\", \"value\": getRandomNumber() },"
-                     + "{ \"label\": \"June\", \"value\": getRandomNumber() },"
-                     + "{ \"label\": \"July\", \"value\": getRandomNumber() },"
-                     + "{ \"label\": \"August\", \"value\": getRandomNumber() },"
-                     + "{ \"label\": \"September\", \"value\": getRandomNumber() },"
-                     + "{ \"label\": \"October\", \"value\": getRandomNumber() },"
-                     + "{ \"label\": \"November\", \"value\": getRandomNumber() },"
-                     + "{ \"label\": \"December\", \"value\":getRandomNumber() }" + "]" + "}" + "]" + "}";
+    public string Dashboard()
+    {
+      var output = "{ data: { visitsPerChannel: [ " + "{ \"label\": \"LandingPages\", \"value\": " + GetRandomNumber()
+                   + " }," + "{ \"label\": \"RefURLs\", \"value\": " + GetRandomNumber() + " },"
+                   + "{ \"label\": \"Search\", \"value\": " + GetRandomNumber() + " },"
+                   + "{ \"label\": \"Campaigns\", \"value\": " + GetRandomNumber() + " },"
+                   + "{ \"label\": \"Other\", \"value\": " + GetRandomNumber() + " }" + "]," + "monthlyDistribution: ["
+                   + "{" + "key: \"Monthly Distribution\"," + "values: [" + "{ \"label\": \"January\", \"value\": "
+                   + GetRandomNumber() + " }," + "{ \"label\": \"February\", \"value\": " + GetRandomNumber() + " },"
+                   + "{ \"label\": \"March\", \"value\":" + GetRandomNumber() + " },"
+                   + "{ \"label\": \"April\", \"value\": " + GetRandomNumber() + " },"
+                   + "{ \"label\": \"Maj\", \"value\": " + GetRandomNumber() + " },"
+                   + "{ \"label\": \"June\", \"value\": " + GetRandomNumber() + " },"
+                   + "{ \"label\": \"July\", \"value\": " + GetRandomNumber() + " },"
+                   + "{ \"label\": \"August\", \"value\": " + GetRandomNumber() + " },"
+                   + "{ \"label\": \"September\", \"value\": " + GetRandomNumber() + " },"
+                   + "{ \"label\": \"October\", \"value\": " + GetRandomNumber() + " },"
+                   + "{ \"label\": \"November\", \"value\": " + GetRandomNumber() + " },"
+                   + "{ \"label\": \"December\", \"value\":" + GetRandomNumber() + " }" + "]" + "}" + "]" + "}";
 
-        return output;
-      }
-
+      return output;
     }
+
+    private string GetRandomNumber()
+    {
+      return getrandom.Next(0, 100).ToString();
+    }
+  }
 }
